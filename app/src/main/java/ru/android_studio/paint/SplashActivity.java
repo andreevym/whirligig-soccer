889abe743ab8b85c253a1d1aa00d81f41aad720d
andreevym <Yury.Andreev@ru.ibm.com>
2017-03-13 23:03:53 +0300

    добавил экран загрузки

---
+++ app/src/main/java/ru/android_studio/paint/SplashActivity.java
@@ -0,0 +1,53 @@
+package ru.android_studio.paint;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Заставка на экране перед загруженным приложением
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        startHeavyProcessing();
+
+    }
+
+    private void startHeavyProcessing(){
+        new LongOperation().execute("");
+    }
+
+    private class LongOperation extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            //some heavy processing resulting in a Data String
+            for (int i = 0; i < 3; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.interrupted();
+                }
+            }
+            return "whatever result you have";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        @Override
+        protected void onPreExecute() {}
+
+        @Override
+        protected void onProgressUpdate(Void... values) {}
+    }
+}
